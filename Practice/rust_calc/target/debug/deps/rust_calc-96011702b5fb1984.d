523173bbbf7d2b0c0e319c75902184af0ea90b1f
Strikkin <138825623+Strikkin@users.noreply.github.com>
2023-07-11 01:48:28 +0000

    added rust-calc as practice

---
+++ Practice/rust_calc/target/debug/deps/rust_calc-96011702b5fb1984.d
@@ -0,0 +1,5 @@
+/workspaces/Practice-Learning-Rust/Practice/rust_calc/target/debug/deps/rust_calc-96011702b5fb1984: src/main.rs
+
+/workspaces/Practice-Learning-Rust/Practice/rust_calc/target/debug/deps/rust_calc-96011702b5fb1984.d: src/main.rs
+
+src/main.rs:
---
+++ Practice/rust_calc/target/debug/rust_calc.d
@@ -0,0 +1,1 @@
+/workspaces/Practice-Learning-Rust/Practice/rust_calc/target/debug/rust_calc: /workspaces/Practice-Learning-Rust/Practice/rust_calc/src/main.rs
